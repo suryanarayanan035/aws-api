9ecb39d6c6cab284c3a5603a511c82c26af7065f
suryanarayanan035 <32085008+suryanarayanan035@users.noreply.github.com>
2020-11-20 05:29:18 -0800

    Update CreateEmployeeLambda.js

--- CreateEmployeeLambda.js
+++ CreateEmployeeLambda.js
@@ -1,32 +1,36 @@
 const AWS = require("aws-sdk");
 const documentClient = new AWS.DynamoDB.DocumentClient();
+
 exports.handler = async (event) => {
+    // TODO implement
     
-    const {empid,name,role} = JSON.parse(event.body)
-    
-    const params = {TableName:"employees",
+    const {empid,name,salary} = JSON.parse(event.body);
+    const params = {
+        TableName:"employee",
         Item:{
             empid:empid,
             name:name,
-            role:role
+            salary:salary
         }
     };
     try {
-        
-    await documentClient.put(params).promise();
-    const response = {
-        statusCode: 200,
-        body: "Data inserted successfully",
-    };
-    return response;
-    } catch (e) {
-        
+        await documentClient.put(params).promise();
         const response = {
-        statusCode: 500,
-        body: JSON.stringify(e),
-    };
-    return response;
+            statusCode: 200,
+            body: JSON.stringify('Data saved successfully')
+            
+        }
+        ;
+        return response;
+    } catch (e) {
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify('Data not saved')
+            
+        };
         
+        return response;
     }
-    
+  
+  
 };
